23709c974f4de3c5b7101530cf2dfb03f66e79f3
osbornetunde <osbornetunde@gmail.com>
2019-05-19 10:32:21 +0100

    implemented sort option

--- public/products.js
+++ public/products.js
@@ -11,12 +11,14 @@ class ProductList extends React.Component {
             faces: [],
             page: 1,
             loading: false,
-            prevY: 0
+            sorting: false,
+            prevY: 0,
+            sort: ""
         } 
     }
 
 componentDidMount() {
-    this.getfaces(this.state.page);
+    this.getfaces(this.state.page) 
     
     //options
     let options = {
@@ -54,45 +56,34 @@ handleObserver( entities, observer) {
         this.setState({ loading: true })
         const response = await axios.get("http://localhost:3000/products?",{
              params: {
+                 _sort: this.state.sort,
                  _page: page,
                  _limit: 15
              }
          })
-        //  console.log(response)
+        
             this.setState({faces: [...this.state.faces, ...response.data]})
             this.setState({ loading: false })
+            this.setState({ sorting: false })
         }
     
-    // sortHandler = async (size, price, id) => {
-    //     this.setState({ loading: true })
-    //     const response = await axios.get("http://localhost:3000/products?",{
-    //          params: {
-    //              _sort: size || price || id,
-    //              _limit: 15
-    //          }
-    //      })
-    //      this.setState({faces: [...this.state.faces, ...response.data]})
-    //      this.setState({ loading: false })
-    // }
-
-
-    
-
-    
-
-    setIdHandler = e => {
-        this.setState({id: "id"})
+    setSortHandler = (e) => {
+        
+        this.setState({ faces: [] })
+        this.setState({ sorting: true })
+        this.setState({ sort: e.target.value })
     }
 
-    // // Helper function to render adverts
-    // renderAdverts = () => {
-           
-    //     {this.state.faces.length % 20 === 0 ? 
-    //         <img className="ad" src={`/ads/?r=${randomNumber} + `}/> :
-    //         null
-    //         }
+
+    // Helper function to render adverts
+    renderAdverts = () => {
+        const randomNumber = Math.floor(Math.random() * 1000)
+        {this.state.faces.length % 20 === 0 ? 
+            <img className="ad" src={`/ads/?r=${randomNumber} + `}/> :
+            null
+            }
         
-    // };
+    };
       
     // Helper function to render faces
     renderContent = () => {
@@ -103,10 +94,10 @@ handleObserver( entities, observer) {
                     key={face.id} 
                     className="card"
                 >
-                    <div className="card__items">{face.face}</div>
+                <div className="card__items">{face.face}</div>
                 <div className="card__details">
-                    <p>{`size: ${face.size}px`}</p>
-                    <p>{`price: $${(face.price/100).toFixed(2)}`}</p>
+                    <p>{`size: ${face.size}px`}</p>  {/*add pixels to the size*/}
+                    <p>{`price: $${(face.price/100).toFixed(2)}`}</p>  {/*formats the price to 2 decimal places*/}
                     <p>{face.date}</p>
                 </div>
                 </div>
@@ -117,32 +108,57 @@ handleObserver( entities, observer) {
     render() {
         const randomNumber = Math.floor(Math.random() * 1000)
         const loadingfaces = { display: this.state.loading ? "block" : "none"}
-        const loadingAdverts = { display: this.state.faces.length % 20 === 0 ? "block" : "none"}
-    return(
-        <div className="container">
-        <div className="cards">
-            {this.renderContent()}
-        </div>
-        <img className={loadingAdverts} src={`/ads/?r=${randomNumber} + `}/>
-        <div 
-            ref={loadingRef => (this.loadingRef = loadingRef)}
-            style={{"height": "100px", "margin": "1rem"}}
-        >
-            <span className="loading" style={loadingfaces}>
-                <h1>
-                    <span>L</span>
-                    <span>o</span>
-                    <span>a</span>
-                    <span>d</span>
-                    <span>i</span>
-                    <span>n</span>
-                    <span>g</span>
-                </h1>
-            </span>
-        </div>
-        {this.state.faces.length === 500 ? <p className="end">End of Catalogue</p> : null}
-        </div>
-    )
+        return(
+            <div className="container">
+                <div className="options">
+                    <span>Sort according to Prize, Date or Size</span>
+                    <select className="field" onChange={this.setSortHandler} value={this.state.sort}>
+                        <option value="" disabled hidden>Choose here</option>
+                        <option>size</option>
+                        <option>price</option>
+                        <option>date</option>
+                    </select>
+                    {/* <button onClick={this.getfaces}>Sort</button> */}
+                </div>
+                {this.state.sorting ? <div className={loadingfaces}>sorting</div> :
+                    <div className="cards">
+                        {this.renderContent()}
+                    </div>
+                }
+                    {/*Condition insert an advert when the number of item on the screen
+                    is a factor of 20 */}
+                    {this.state.faces.length % 20 === 0 ?
+                    <img className="ads" src={`/ads/?r=${randomNumber} + `}/> :
+                    null}
+
+                    {/*Loadingref is a reference point that signals to the observer fuction that
+                    we have gotten to the end of the screen*/}
+                    <div 
+                        ref={loadingRef => (this.loadingRef = loadingRef)}
+                        style={{"height": "100px", "margin": "1rem"}}
+                    >
+
+                        {/*The Loading animation markup*/}
+                            <span className="loading" style={loadingfaces}>
+                                <h1>
+                                    <span>L</span>
+                                    <span>o</span>
+                                    <span>a</span>
+                                    <span>d</span>
+                                    <span>i</span>
+                                    <span>n</span>
+                                    <span>g</span>
+                                </h1>
+                            </span>
+                    </div>
+
+                    {/*This displays the "end of the catalogue" once all 
+                    the items have been displayed*/}
+                    {this.state.faces.length === 500 ? <p className="end">End of Catalogue</p> : null}
+                    
+                
+            </div>
+        )
     }
 }
 
